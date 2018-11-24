e376667d522816c8b31c34d348a9babd1a16d5d2
Georges Gauthier <13079044+glgauthier@users.noreply.github.com>
2018-11-24 17:16:36 -0500

    added existing content

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+			$('#fullpage').fullpage({
+                anchors: ['home', 'about', 'R&D','HAL','CurrentDesigns','Finger','HelpingHand','FurtherResearch'],
+                navigation: true,
+                navigationPosition: 'right',
+
+			});
+		});
